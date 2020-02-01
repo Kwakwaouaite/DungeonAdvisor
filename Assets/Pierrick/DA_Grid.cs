bd8ed8147c4831cac647a0e42a025061857600ee
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2020-02-01 14:45:45 +0100

    Begin creation of pathfinding

---
+++ Assets/Pierrick/DA_Grid.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell
+{
+    DA_Object m_Object;
+
+    public virtual bool IsWalkable()
+    {
+        return true;
+    }
+
+    public virtual bool HaveStuff()
+    {
+        return m_Object != null;
+    }
+}
+
+public class BrokenFloor : Cell
+{
+    public override bool IsWalkable()
+    {
+        return false;
+    }
+}
+
+public class DA_Grid : MonoBehaviour
+{
+    public int m_Height = 6;
+    public int m_Width = 8;
+
+    public Cell[,] m_Cells;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 offset;
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < m_Width+1; i++)
+        {
+            offset = Vector3.right * i;
+            Gizmos.DrawLine(transform.position + offset, transform.position + offset + Vector3.up * m_Height);
+        }
+
+        for (int i = 0; i < m_Height + 1; i++)
+        {
+            offset = Vector3.up * i;
+            Gizmos.DrawLine(transform.position + offset, transform.position + offset + Vector3.right * m_Width);
+        }
+
+        if (!Application.IsPlaying(this))
+        {
+            return;
+        }
+
+        offset = Vector3.right * 0.5f + Vector3.down * 0.5f;
+
+        for (int i = 0; i < m_Cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < m_Cells.GetLength(1); j++)
+            {
+                Gizmos.color = m_Cells[i, j].IsWalkable() ? Color.green : Color.red;
+
+                Gizmos.DrawSphere(transform.position + offset + Vector3.right * i + Vector3.down * j, 0.5f);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        GenerateRandomGrid();
+    }
+
+    private void GenerateRandomGrid()
+    {
+        m_Cells = new Cell[m_Width, m_Height];
+
+        for (int i = 0; i < m_Cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < m_Cells.GetLength(1); j++)
+            {
+                float rand = Random.Range(0.0f, 1.0f);
+                if (rand < 0.8f)
+                {
+                    m_Cells[i, j] = new Cell();
+                }
+                else
+                {
+                    m_Cells[i, j] = new BrokenFloor();
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateRandomGrid();
+        }
+    }
+}
---
+++ Assets/Pierrick/Pathfinding.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public class ObjectPathData
+    {
+        public DA_Object objectFound;
+        public Vector2[] path;
+    }
+
+    public static readonly Vector2Int[] Directions = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
+
+    public ObjectPathData[] FindAccessibleObjects(DA_Grid grid, Vector2Int StartingPoint)
+    {
+        ObjectPathData[] ObjectFound;
+
+        Queue<Vector2Int> NextIteration = new Queue<Vector2Int>();
+        Queue<Vector2Int> CurrentIteration = new Queue<Vector2Int>();
+
+        ObjectPathData[,] fakeGrid = new ObjectPathData[grid.m_Width, grid.m_Height];
+
+        NextIteration.Enqueue(StartingPoint);
+
+        while( NextIteration.Count > 0)
+        {
+            CurrentIteration = NextIteration;
+            NextIteration = new Queue<Vector2Int>();
+
+            Vector2Int currentNode = CurrentIteration.Dequeue();
+
+            foreach (Vector2Int direction in Directions)
+            {
+                Vector2Int potentialNode = currentNode + direction;
+
+                if (IsValidPosition(potentialNode, grid))
+                {
+                    if (fakeGrid[potentialNode.x, potentialNode.y] != null)
+                    {
+                        continue;
+                    }
+
+                    ObjectPathData newPathData = new ObjectPathData();
+                    Cell potentialCell = grid.m_Cells[potentialNode.x, potentialNode.y];
+
+                    if (potentialCell.IsWalkable())
+                    {
+                        //newPathData
+                    }
+                }
+
+            }
+        }
+
+        return null;
+    }
+
+    private bool IsValidPosition(Vector2 position, DA_Grid grid)
+    {
+        if (0 < position.x && position.x < grid.m_Width
+            && 0 < position.y && position.y < grid.m_Height)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
